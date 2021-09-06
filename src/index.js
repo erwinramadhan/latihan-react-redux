194ef43ff208d07b3a74eb649a9691eb2535b04c
Erwin Ramadhan <erwinramadhan35@gmail.com>
2021-09-06 19:02:27 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import store from './store';
+import history from './utils/history';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CounterContainer from './containers/CounterContainer';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route exact path="/" component={App}/>
+          <Route exact path="/counter" component={CounterContainer}/>
+          <Redirect from="*" to="/" />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
